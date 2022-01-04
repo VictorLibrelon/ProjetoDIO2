3ff1b2e7c8ed4aaba21efd62bcc26f45912cf7a0
Victor Librelon Inacio <86538418+VictorLibrelon@users.noreply.github.com>
2022-01-04 17:31:19 -0300

    desafio da DIO

---
+++ POO/ExemploPOO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Valores validos
+            Retangulo r = new Retangulo();
+            r.DefinirMedidas(30,30);
+
+            System.Console.WriteLine($"Área: {r.ObterArea()}");
+
+
+
+
+            // Pessoa P1 = new Pessoa();
+
+            // P1.Nome = "Bob";
+            // P1.Idade = 20;
+
+            // P1.Apresentar();
+            
+        }
+
+    }
+}
